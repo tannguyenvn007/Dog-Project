a3409977f75aa5c09003aa178ac9f3f1310e82cb
quyen.nguyen <quyen.nguyen@pn.com.vn>
2019-01-14 11:34:28 +0700

    finish register with API

--- app/src/main/java/com/example/tannguyen/moneyapplication/Actions.java
+++ app/src/main/java/com/example/tannguyen/moneyapplication/Actions.java
@@ -7,17 +7,21 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.LogInCallback;
 
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class Actions {
     public void parseConnection(Context context) {
         Parse.initialize(new Parse.Configuration.Builder(context)
-                .applicationId("xmveNrevqQ6DodFaaYB5BdLlQrFa96iKf8QLBhvr")
-                .clientKey("dKUykxREF3X4THVMzXs65CfKntv9JndoPrwWroEW")
+                .applicationId("n5k0cNPdmW1kLFInNlKdoGusTJ5WE9Gg6nL8uc3R")
+                .clientKey("Wd81NWpxXIN2mES8tgPjmupI4dbOWBublRc2eyaN")
                 .server("https://parseapi.back4app.com")
                 .build()
         );
@@ -37,6 +41,30 @@ public class Actions {
             }
         });
         return 0;
+    }
+
+    public boolean register(final Context context, final String username, final String password, String phone, String indentifyCartNumber , String confirmPassword, String address){
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.put("indentifyCartNumber", indentifyCartNumber);
+        user.put("address", address);
+        user.put("phone", phone);
+        user.put("confirmPassword", confirmPassword);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    System.out.print("Ok");
+                    Log.i("QuyenApplication", "Oki");
+                    login(context, username, password);
+                } else {
+                    System.out.print("Failed");
+                    Log.i("QuyenApplication", "Notoki");
+                }
+            }
+        });
+        return true;
     }
 
     public String currentUser(final Context context){
@@ -83,5 +111,19 @@ public class Actions {
             Toast.makeText(context,"Phiên làm việc đã hết hạn. Đăng nhập lại.",Toast.LENGTH_SHORT).show();
         }
         return true;
+    }
+
+    public void getUsers(Context context, String objectID){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+
+        query.getInBackground(objectID, new GetCallback<ParseObject>() {
+            public void done(ParseObject result, ParseException e) {
+                if (e == null) {
+                    System.out.println(result);
+                } else {
+                    // something went wrong
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tannguyen/moneyapplication/Register.java
+++ app/src/main/java/com/example/tannguyen/moneyapplication/Register.java
@@ -1,13 +1,76 @@
 package com.example.tannguyen.moneyapplication;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class Register extends AppCompatActivity {
 
+    Actions actions = new Actions();
+    EditText register_etxtFullName, register_etxtemail, register_etxtPhone,register_etxtAddress, register_etxtCardNumber, register_etxtPassword, register_etxtConfirmPassword  ;
+    Button button_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        actions.parseConnection(Register.this);
+
+        register_etxtFullName = (EditText) findViewById(R.id.register_etxtFullName);
+        register_etxtPhone = (EditText) findViewById(R.id.register_etxtPhone);
+        register_etxtAddress = (EditText) findViewById(R.id.register_etxtAddress);
+        register_etxtCardNumber = (EditText) findViewById(R.id.register_etxtCardNumber);
+        register_etxtPassword = (EditText) findViewById(R.id.register_etxtPassword);
+        register_etxtConfirmPassword = (EditText) findViewById(R.id.register_etxtConfirmPassword);
+        button_register = (Button) findViewById(R.id.button_register);
+
+
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (register_etxtFullName.getText().toString().equals("")
+                        || register_etxtPhone.getText().toString().equals("")
+                        || register_etxtAddress.getText().toString().equals("")
+                        || register_etxtCardNumber.getText().toString().equals("")
+                        || register_etxtPassword.getText().toString().equals("")
+                        || register_etxtConfirmPassword.getText().toString().equals("")){
+                    Toast.makeText(Register.this,"All of filed is required.",Toast.LENGTH_SHORT).show();
+                }else {
+//
+                    if (register_etxtPassword.getText().toString().equals(register_etxtConfirmPassword.getText().toString())){
+                        actions.parseConnection(Register.this);
+                        boolean re = actions.register(Register.this, register_etxtFullName.getText().toString(),
+                                register_etxtPassword.getText().toString(),
+                                register_etxtPhone.getText().toString(),
+                                register_etxtCardNumber.getText().toString(),
+                                register_etxtConfirmPassword.getText().toString(),
+                                register_etxtAddress.getText().toString());
+
+
+                        if (re){
+                            Log.i("QuyenApplication", "Okkk");
+                        }else {
+                            Log.i("QuyenApplication", "Failde");
+
+                        }
+                    }else {
+                        Toast.makeText(Register.this,"Confrim password is wrong. "+register_etxtPassword.getText()+" and " +register_etxtConfirmPassword.getText(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
+
 }
